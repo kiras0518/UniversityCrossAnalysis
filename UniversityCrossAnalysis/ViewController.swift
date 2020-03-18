cfbb53fc77e094d56d3bfc322e0b2aa0d1e334c3
kiras <kiras0518@gmail.com>
2020-03-18 22:47:03 +0800

    Initial Commit

---
+++ UniversityCrossAnalysis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UniversityCrossAnalysis
+//
+//  Created by kiras on 2020/3/18.
+//  Copyright © 2020 ameyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
